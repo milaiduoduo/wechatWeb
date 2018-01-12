0014abef4594294f217ecba1ba1cc07fe16917ba
advancingKoala <milaiduoduo4567@163.com>
2018-01-12 22:38:31 +0800

    增加access_token获取逻辑代码，并成功的获取到access_token

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 'use strict';
 
 let Koa = require('koa');
+const path = require('path');
 let sha1 = require('sha1');
-let wechat = require('./wechat/generator_1')
+let wechat = require('./wechat/generator_1');
+let util = require('./libs/util')
 let app = new Koa();
-
+let wechat_file = path.join(__dirname,'./config/wechat.txt');
 let config = {
     wechat:{
         appID:'wx2bda83f75086e961',
         appSecret:'ee6d13d929cd0caf54316bca45809f29',
-        token:'xingmaxiaobao'
+        token:'xingmaxiaobao',
+        getAccessToken:function(){
+            return util.readFileAsync(wechat_file);
+        },
+        saveAccessToken:function(data){
+            data = JSON.stringify(data);
+            return util.writeFileAsync(wechat_file,data);
+        }
     }
 }
 
