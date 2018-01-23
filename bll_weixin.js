a64500cb096cd1d68da85c4c33258e85dbc0535e
advancingKoala <milaiduoduo4567@163.com>
2018-01-23 11:59:04 +0800

    【上传临时图片】，重写fectchAccessToken, 成功。 重写uploadTempMaterial，大坑，没写最后的return，导致then方法一直无法调用。目前卡在post方法上。

--- bll_weixin.js
+++ bll_weixin.js
@@ -1,13 +1,17 @@
 'use strict'
-exports.setReplyContent = function* (next){
+
+const config = require('./config');
+const Wechat = require('./wechat/wechat');
+var wechatApi = new Wechat(config.wechat);
+
+exports.setReplyContent = function*(next) {
     //console.log('in handler!!!!!!');
     var receivedMessage = this.receivedMessage;
-    console.log('receivedMessage:::',receivedMessage);
-    if(receivedMessage.MsgType === 'event'){
-        switch(receivedMessage.Event)
-        {
+    console.log('receivedMessage:::', receivedMessage);
+    if (receivedMessage.MsgType === 'event') {
+        switch (receivedMessage.Event) {
             case 'subscribe':
-                if(receivedMessage.EventKey){
+                if (receivedMessage.EventKey) {
                     console.log(`扫二维码进来：${receivedMessage.EventKey} ${receivedMessage.ticket}`);
                 }
                 this.body = '哈哈哈哈哈，终于等到你，我都等哭了!!!\r\n';
@@ -27,48 +31,53 @@ exports.setReplyContent = function* (next){
                 this.body = '看到你扫了一下哦！';
                 break;
             case 'VIEW':
-                this.body = '您点击了菜单中的链接：'+receivedMessage.EventKey;
+                this.body = '您点击了菜单中的链接：' + receivedMessage.EventKey;
         }
     }
-    else if(receivedMessage.MsgType === 'text'){
+    else if (receivedMessage.MsgType === 'text') {
         let receivedContent = receivedMessage.Content;
         let reply = '';
-        switch(receivedContent){
+        switch (receivedContent) {
             case '1':
                 reply = '一只小马耍滑梯';
                 break;
             case '2':
-                reply = '两只小马点灯亮';
+                console.log('__dirname:::',__dirname);
+                let data = yield wechatApi.uploadTempMaterial('image', __dirname + '/public/temp1.jpg')
+                reply = {
+                    type: 'image',
+                    mediaId: data.mediaId
+                }
                 break;
             case '3':
                 reply = '三只小马爬雪山';
                 break;
             case '4':
                 reply = [{
-                    title:'Node.js的世界',
-                    description:'只是个描述而已',
-                    picUrl:'https://gd4.alicdn.com/imgextra/i1/TB1GmEWPVXXXXbgaXXXYXGcGpXX_M2.SS2',
-                    url:'https://github.com/'
-                },{
-                    title:'koa2的世界',
-                    description:'也只是个描述而已',
-                    picUrl:'http://ss.csdn.net/p?http://mmbiz.qpic.cn/mmbiz_png/Inx1Hb2kt6ZCDZX1zL0BZHiayA0aJDZWvSUd3k6vGUcFXE8AJiaDqeicWoAPu0HvaP3cxy9rm2UTEfv81CXK4PBZQ/640?wx_fmt=png&wxfrom=5&wx_lazy=1',
-                    url:'https://www.baidu.com'
-                },{
-                    title:'微信的世界',
-                    description:'也只是个描述而已',
-                    picUrl:'http://pic137.nipic.com/file/20170801/23159666_200250564035_2.jpg',
-                    url:'https://www.baidu.com'
-                },{
-                    title:'Promise的世界',
-                    description:'也只是个描述而已',
-                    picUrl:'http://pic111.nipic.com/file/20160929/24014891_091758419000_2.jpg',
-                    url:'https://www.baidu.com'
-                },{
-                    title:'Generator的世界',
-                    description:'也只是个描述而已',
-                    picUrl:'http://pic128.nipic.com/file/20170504/25230470_161040886038_2.jpg',
-                    url:'https://www.baidu.com'
+                    title: 'Node.js的世界',
+                    description: '只是个描述而已',
+                    picUrl: 'https://gd4.alicdn.com/imgextra/i1/TB1GmEWPVXXXXbgaXXXYXGcGpXX_M2.SS2',
+                    url: 'https://github.com/'
+                }, {
+                    title: 'koa2的世界',
+                    description: '也只是个描述而已',
+                    picUrl: 'http://ss.csdn.net/p?http://mmbiz.qpic.cn/mmbiz_png/Inx1Hb2kt6ZCDZX1zL0BZHiayA0aJDZWvSUd3k6vGUcFXE8AJiaDqeicWoAPu0HvaP3cxy9rm2UTEfv81CXK4PBZQ/640?wx_fmt=png&wxfrom=5&wx_lazy=1',
+                    url: 'https://www.baidu.com'
+                }, {
+                    title: '微信的世界',
+                    description: '也只是个描述而已',
+                    picUrl: 'http://pic137.nipic.com/file/20170801/23159666_200250564035_2.jpg',
+                    url: 'https://www.baidu.com'
+                }, {
+                    title: 'Promise的世界',
+                    description: '也只是个描述而已',
+                    picUrl: 'http://pic111.nipic.com/file/20160929/24014891_091758419000_2.jpg',
+                    url: 'https://www.baidu.com'
+                }, {
+                    title: 'Generator的世界',
+                    description: '也只是个描述而已',
+                    picUrl: 'http://pic128.nipic.com/file/20170504/25230470_161040886038_2.jpg',
+                    url: 'https://www.baidu.com'
                 }]
 
                 break;
--- wechat/wechat.js
+++ wechat/wechat.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const fs = require('fs');
 const request = require('request');
 const util = require('./util');
 
 const prefix = 'https://api.weixin.qq.com/cgi-bin/';
 const api = {
-    accessToken: `${prefix}token?grant_type=client_credential`
+    accessToken: `${prefix}token?grant_type=client_credential`,
+    uploadTempMaterialUrl:prefix+'media/upload?'
 }
 function Wechat(opts) {
     var that = this;
@@ -14,23 +16,51 @@ function Wechat(opts) {
     this.appSecret = opts.appSecret;
     this.getAccessToken = opts.getAccessToken;
     this.saveAccessToken = opts.saveAccessToken;
-    this.getAccessToken().then(function (data) {
-        if (that.isValidAccessToken(data)) {
-            that.access_token = data.access_token;
-            that.expires_in = data.expires_in;
+    this.fetchAccessToken();
+
+}
+
+Wechat.prototype.test = function(){
+    this.fetchAccessToken().then(function(data){
+        console.log('fetchAccessToken回调参数值：',data);
+    });
+}
+
+Wechat.prototype.fetchAccessToken = function (data) {
+    let that = this;
+    let tokenVaildFlag = false;
+    console.log('this.access_token && this.expires_in:',this.access_token, this.expires_in);
+    if(this.access_token && this.expires_in){
+        if(this.isvalidAccessToken(this)){
+            console.log('提前验证成功，返回Promise.resolve(this)');
+            return Promise.resolve(this);
+        }
+    }
+
+    return this.getAccessToken().then(function (data) {
+        tokenVaildFlag = that.isValidAccessToken(data);
+        if (tokenVaildFlag) {
+            console.log('验证成功，只需要返回data！！！')
             return data;
         }
         else {
+            console.log('验证失败，需要重新请求new token！！！')
             return that.updateAccessToken();
         }
-    }).then(function(data){
+    }).then(function (data) {
         // 是updataAccessToken的回调
         that.access_token = data.access_token;
         that.expires_in = data.expires_in;
-        that.saveAccessToken(data);
-    }).catch(function (e) {
-        console.log('err in getAccessToken!!!!!:', e);
+        if (!tokenVaildFlag) {
+            console.log('验证失败，重新保存token！！！')
+            that.saveAccessToken(data);
+        }
+        console.log('最后会在这里返回，Promise.resolve(data)');
+        return Promise.resolve(data);
     })
+    // .catch(function (e) {
+    //     console.log('err in getAccessToken!!!!!:', e);
+    // })
 }
 
 Wechat.prototype.isValidAccessToken = function (data) {
@@ -54,7 +84,7 @@ Wechat.prototype.isValidAccessToken = function (data) {
 
 Wechat.prototype.updateAccessToken = function () {
     let url = `${api.accessToken}&appid=${this.appID}&secret=${this.appSecret}`;
-    console.log('url:::',url);
+    console.log('url:::', url);
     return new Promise(function (resolve, reject) {
         request.get({url: url, json: true}, function (error, response, body) {
             let data = body;
@@ -66,13 +96,47 @@ Wechat.prototype.updateAccessToken = function () {
     })
 }
 
-Wechat.prototype.reply = function(){
+Wechat.prototype.uploadTempMaterial = function(type,filepath){
+    let that = this;
+    let form = {
+        media: fs.createReadStream(filepath)
+    };
+    console.log('in uploadTempMaterial 上传素材方法中,media！！！',form.media);
+    return new Promise(function(resolve,reject){
+       //console.log('that.fetchAccessToken()：：：：：：：：',that.fetchAccessToken());
+        that.fetchAccessToken().then(function(data){
+            let url = api.uploadTempMaterialUrl + 'access_token=' + data.access_token + '&type=' + type;
+            request.post({url: url,form:form, json: true}, function (error, response, body) {
+                let _data = body;
+                if(!error){
+                    if(_data){
+                        console.log('post返回的结果是？:',body);
+                        return Promise.resolve(_data);
+                        //resolve(_data)
+                    }
+                    else{
+                        return Promise.reject(error);
+                        // throw new Error('upload temporary material failed!!!!')
+                    }
+                }
+                return Promise.reject(error);
+
+
+            })
+            //     .catch(function(err){
+            //     reject(err);
+            // })
+        })
+    })
+}
+
+Wechat.prototype.reply = function () {
     var sendContent = this.body;
     var message = this.receivedMessage;
 
 
-    var xml = util.tpl(sendContent,message);
-    console.log('回复的xml:',xml);
+    var xml = util.tpl(sendContent, message);
+    console.log('回复的xml:', xml);
     this.status = 200;
     this.type = 'application/xml';
     this.body = xml;
